8a8d3258f82d98c90ef29db008f825a37314f7e3
Giantt <giantt67@gmail.com>
2025-08-04 03:12:19 -0400

    Game Over Advance

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -19,6 +19,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake()
     {
@@ -76,6 +77,8 @@ public class DeliveryManager : MonoBehaviour
                 if (plateContentsMatchesRecipe)
                 {
                     // Player delivered the correct recipe
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -88,9 +91,14 @@ public class DeliveryManager : MonoBehaviour
         // Player delivered the wrong recipe
         OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
-    
+
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList;
+    }
+    
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 }
---
+++ Assets/Scripts/UI/GamePlayingClockUI.cs
@@ -0,0 +1,15 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayingClockUI : MonoBehaviour
+{
+
+    [SerializeField] private UnityEngine.UI.Image timerImage;
+
+    private void Update()
+    {
+        timerImage.fillAmount = KitchenGameManager.Instance.GetGamePlayingTimerNormalized();
+    }
+
+}
